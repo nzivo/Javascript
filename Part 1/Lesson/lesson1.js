bda1047f4fa794b7a0ea0e3fa87c699db621a162
nzivo <johnnnzivo@gmail.com>
2021-02-24 15:22:23 +0300

    Lesson 24: Logical operators

--- Part 1/Lesson/lesson1.js
+++ Part 1/Lesson/lesson1.js
@@ -7,7 +7,7 @@ assign their values according to your own country (population in millions)
 */
 let country = "Kenya";
 let continent = "Africa";
-let population = 4800000;
+let population = 48000000;
 
 // Log their values to the console
 
@@ -124,18 +124,41 @@ when you input 1
 8. Reflect on why we should use the === operator and type conversion in this
 situation
 */
-const numNeighbours = Number(prompt(`How many neighbour countries does your country have ?`));
-// if(numNeighbours == 1) console.log(`Only 1 border`);
+// const numNeighbours = Number(prompt(`How many neighbour countries does your country have ?`));
+// // if(numNeighbours == 1) console.log(`Only 1 border`);
 
-if(numNeighbours === 1){
-    console.log(`Only 1 border`);
-}else if (numNeighbours > 1) {
-    console.log(`Only ${numNeighbours} borders`);
-}else {
-    console.log(`No borders`);
+// if(numNeighbours === 1){
+//     console.log(`Only 1 border`);
+// }else if (numNeighbours > 1) {
+//     console.log(`Only ${numNeighbours} borders`);
+// }else {
+//     console.log(`No borders`);
+// }
+
+
+/*
+===========================================================================
+                 TASK 
+===========================================================================
+1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island.
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in
+Canada :D)
+
+*/
+if ((language === `English`) && (population < 50000000) && !isIsland ) {
+    console.log(`You should live in ${country} :)`);
+} else {
+    console.log(`${country} does not meet your criteria`);
 }
-
-
 
 
 /*
