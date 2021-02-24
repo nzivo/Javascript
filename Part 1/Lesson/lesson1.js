929061a1e07399040fa0184bab4c04f6a303865a
nzivo <johnnnzivo@gmail.com>
2021-02-24 16:20:09 +0300

    Lesson 28: Conditional (ternary) Operator

--- Part 1/Lesson/lesson1.js
+++ Part 1/Lesson/lesson1.js
@@ -222,6 +222,20 @@ switch (language) {
 
 /*
 ===========================================================================
+                 TASK 10
+===========================================================================
+1. If your country's population is greater than 33 million, use the ternary operator
+to log a string like this to the console: 'Portugal's population is above average'.
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
+
+console.log(`${population > 33000000 ? `${country}'s population is above average` : `${country}'s population is below average`}`);
+
+/*
+===========================================================================
                  TASK 
 ===========================================================================
 
