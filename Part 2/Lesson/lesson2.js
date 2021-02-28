1eda8e2c072b47766bfd0aae78c196a104e3b625
nzivo <johnnnzivo@gmail.com>
2021-02-28 17:24:25 +0300

    For Loop

--- Part 2/Lesson/lesson2.js
+++ Part 2/Lesson/lesson2.js
@@ -215,5 +215,19 @@ myCountry1.checkIsland();
 ===========================================================================
                  TASK 
 ===========================================================================
+1. There are elections in your country! In a small town, there are only 50 voters.
+Use a for loop to simulate the 50 people voting, by logging a string like this to
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+*/
+let voter;
+for (voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`); 
+};
+
+
+/*
+===========================================================================
+                 TASK 
+===========================================================================
 
 */
