9be590668261f30f5e8a8d9b885f2b13f90aaae9
nzivo <johnnnzivo@gmail.com>
2021-02-28 15:11:14 +0300

    Methods on Arrays

--- Part 2/Lesson/lesson2.js
+++ Part 2/Lesson/lesson2.js
@@ -115,7 +115,37 @@ const populations = [48, 1441, 30, 15];
 console.log(populations[populations.length - 1] != null); // populations.length === 4
 const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]),
 percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
-console.log(percentages[0], percentages[1], percentages[2], percentages[3] );
+console.log(percentages);
+
+/*
+===========================================================================
+                 TASK 6
+===========================================================================
+1. Create an array containing all the neighbouring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the
+index of the country in the 'neighbours' array, and then use that index to
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+*/
+const neighbours = ['Uganda', 'Tanzania', 'Somalia'];
+neighbours.push('Utopia');
+console.log(neighbours);
+neighbours.pop();
+console.log(neighbours);
+if (!neighbours.includes('Germany')) {
+    console.log('Probably not a central European country :D');
+}
+neighbours[neighbours.indexOf('Tanzania')] = 'Republic of Tanzania';
+console.log(neighbours);
+
 
 /*
 ===========================================================================
